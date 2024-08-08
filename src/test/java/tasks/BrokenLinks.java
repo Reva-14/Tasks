af4a0af7e872b2ea6095601d79dd5af18baea195
Reva-14 <revathythangam93@gmail.com>
2024-08-09 00:59:54 +0530

    Updated code

--- src/test/java/tasks/BrokenLinks.java
+++ src/test/java/tasks/BrokenLinks.java
@@ -14,6 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrokenLinks {
 	public static void main(String[] args) throws IOException {
+		//updated
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 //		options.addArguments("start-maximized");
