7def7fcafac87ce11028d51e797ce99284f365e1
Reva-14 <revathythangam93@gmail.com>
2024-08-09 02:11:08 +0530

    Second Update

--- src/test/java/tasks/BrokenLinks.java
+++ src/test/java/tasks/BrokenLinks.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
+//second update
 
 public class BrokenLinks {
 	public static void main(String[] args) throws IOException {
